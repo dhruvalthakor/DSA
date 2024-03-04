04223c9691891bff3322a1a5a39d3b9f64449e0b
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-03-04 11:32:02 +0530

    h

---
+++ lec6.2/index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<< "*";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+int num=1;
+    for(int i=5;i>=1;i--){
+        for (int k = 5; k>i; k--)
+        {
+         cout<<" ";
+        }
+        
+        for(int j=1;j<=i;j++){
+            cout<<num;
+            
+        }
+        num++;
+        cout<<endl;
+    }
+
+
+}
