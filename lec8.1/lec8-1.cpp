385672ee1a4872a88c9537c05605874323e57309
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-03-11 12:00:31 +0530

    8.1

---
+++ lec8.1/lec8-1.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+
+// void rahul(){
+//   cout << "my name is rahul" <<endl;
+//   cout << "my age is 18" <<endl;
+// }
+
+// int main(){
+//     rahul();
+
+//     return 0;
+// }
+
+void addition(int a,int b){
+   cout <<"addition is " << a+b << endl;
+}
+
+void multi(int a,int b){
+   cout <<"addition is " << a*b << endl;
+}
+
+int main(){
+    addition(5,8);
+    multi(5,8);
+
+    return 0;
+}
